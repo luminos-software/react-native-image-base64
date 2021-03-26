11f9d48cd1d26c986fff32a8d6fc94162b28181a
Emanuel Boros <emanuel13b@gmail.com>
2021-03-26 20:08:01 +0200

    iOS url get with authorization

--- ios/RNImgToBase64.m
+++ ios/RNImgToBase64.m
@@ -9,18 +9,41 @@
 }
 RCT_EXPORT_MODULE()
 
+// #pragma mark getBase64String
+// RCT_EXPORT_METHOD(getBase64String:(NSURL*)url
+//     token:(NSString*)token
+//     resolve:(RCTPromiseResolveBlock)resolve
+//     rejecter:(RCTPromiseRejectBlock)reject
+// ) {
+//     dispatch_async(dispatch_queue_create("image_processing", 0), ^{
+//         NSData* data = [NSData dataWithContentsOfURL:url];
+//         dispatch_async(dispatch_get_main_queue(), ^{
+//             resolve([data base64EncodedStringWithOptions:0]);
+//         });
+//     });
+// }
+
+
 #pragma mark getBase64String
 RCT_EXPORT_METHOD(getBase64String:(NSURL*)url
+    token:(NSString*)token
     resolve:(RCTPromiseResolveBlock)resolve
     rejecter:(RCTPromiseRejectBlock)reject
 ) {
     dispatch_async(dispatch_queue_create("image_processing", 0), ^{
-        NSData* data = [NSData dataWithContentsOfURL:url];
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:finalURL]]; 
+        [request setHTTPMethod:@"GET"];        
+        NSString *authValue = [NSString stringWithFormat:@"Basic %@", token];
+
         dispatch_async(dispatch_get_main_queue(), ^{
-            resolve([data base64EncodedStringWithOptions:0]);
+            
+            resolve([request setValue:authValue forHTTPHeaderField:@"Authorization" base64EncodedStringWithOptions:0]);
         });
     });
 }
 
+
+
 @end
   
+
